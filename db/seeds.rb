56bd5a63bd8a8b56fa4b0a36b03507a0b8bf4454
Michael Bach <migobach@gmail.com>
2018-05-31 16:51:04 -0600

    add home button

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ 5.times do 
+  board = Board.create(
+    genre: Faker::Music.genre
+  )
+  10.times do
+    board.artists.create(
+      name: Faker::Music.band
+    )
+  end
+end
+
+puts "seeded"
