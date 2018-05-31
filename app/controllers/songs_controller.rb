f88a50932f9d2845ee9b72faa88653d6dc85a8fd
Pete Sword <pete.sword@yahoo.com>
2018-05-31 16:17:34 -0600

    built songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,52 @@
 class SongsController < ApplicationController
+  before_action :set_artist
+
   def index
+    @songs = @artist.songs
   end
 
   def show
   end
 
   def new
+    @song = @artist.songs.new
+    render partial: "form"
   end
 
   def edit
+    render partial: "form"
   end
+
+  def create
+    @song = @artist.songs.new(songs_params)
+
+    if @song.save
+      redirect_to [@artist, @song]
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @song.update(song_params)
+      redirect_to [@artist, @song]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song.destroy
+    redirect_to artist_songs_path
+  end
+
+  private
+    def set_artist
+      @artist = Artist.find(params[:artist_id])
+    end
+
+    def songs_params
+      params.require(:song).permit(:name, :rating)
+    end
+end
 end
